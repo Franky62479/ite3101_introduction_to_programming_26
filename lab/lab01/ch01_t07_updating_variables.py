83d38f4a90aedaa21302d7091f871160045656d1
LamYiuChung-2627 <lamychung88@gmail.com>
2026-09-08 02:33:01 +0000

    [skip actions] lab/lab01/ch01_t07_updating_variables.py

--- lab/lab01/ch01_t07_updating_variables.py
+++ lab/lab01/ch01_t07_updating_variables.py
@@ -11,6 +11,6 @@ september_rainfall = 5.16
 october_rainfall = 7.20
 november_rainfall = 5.06
 december_rainfall = 4.06
-
+print(annual_rainfall)
 annual_rainfall = september_rainfall + december_rainfall + november_rainfall + october_rainfall
 print(annual_rainfall)
